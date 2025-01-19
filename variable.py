9434aa73102a6c867a5a710388c7a687bc464482
smile995 <ah.smile995@gmail.com>
2025-01-19 17:46:00 +0600

    Learn about variable

--- variable.py
+++ variable.py
@@ -5,21 +5,30 @@
 x=10
 y=454
 z=234
-print(x,y,z)
+# print(x,y,z)
 myResult=3.65
 myAge=24
 myHeight= 5.4
-print(myAge,myHeight,myResult)
+# print(myAge,myHeight,myResult)
 
 # Daclaring string as variable
 name="Amir Hamza Ismail"
 universityName='CCN university of science and technology'
 department= "CSE"
-print(name, universityName,department)
+# print(name, universityName,department)
 
 # Declaring bolean that store true or false only
 isStudent=True
 married= False
-print(isStudent,married)
+# print(isStudent,married)
+
+# Assign multiple variable
+x,y,z= "Amir","Hamza","Ismail"
+print(x)
+print(y)
+print(z)
+# one value in multible variables
+x=y=z="Multiple Variable"
+print(x,y,z)
 
 
