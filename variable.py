f2ce5a0ca43a7055082a03a2892815b7767a4228
smile995 <ah.smile995@gmail.com>
2025-01-19 17:34:49 +0600

    Exploring python basic to advance

---
+++ variable.py
@@ -0,0 +1,25 @@
+# variable naming style (snake_case, camelCase, PascalCase)
+# Variables are case sensitive
+
+# Declaring number as variable
+x=10
+y=454
+z=234
+print(x,y,z)
+myResult=3.65
+myAge=24
+myHeight= 5.4
+print(myAge,myHeight,myResult)
+
+# Daclaring string as variable
+name="Amir Hamza Ismail"
+universityName='CCN university of science and technology'
+department= "CSE"
+print(name, universityName,department)
+
+# Declaring bolean that store true or false only
+isStudent=True
+married= False
+print(isStudent,married)
+
+
