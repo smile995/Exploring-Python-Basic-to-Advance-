f518fb11037b67db28b90fe4631e806f08598331
smile995 <ah.smile995@gmail.com>
2025-01-21 22:03:32 +0600

    Explored about python type casting

---
+++ typeCasting.py
@@ -0,0 +1,20 @@
+# integer related example
+x = int(1)   # x will be 1 --> int to int
+y = int(2.8) # y will be 2 --> float to int
+z = int("3") # z will be 3 -->string to int
+# print(x,y,z)
+
+# float related example
+
+a = float(1)     # x will be 1.0 --> int to float
+b = float(2.8)   # y will be 2.8 --> float to float
+c = float("3")   # z will be 3.0 --> string to float
+d = float("4.2") # w will be 4.2 --> string to float
+# print(a,b,c,d)
+
+# float related example
+
+m = str("s1") # x will be 's1' --> string to string
+n = str(2)    # y will be '2'  --> int to string
+o = str(3.0)  # z will be '3.0'--> float to string 
+print(m,n,o)
