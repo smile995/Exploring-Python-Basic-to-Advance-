9e2789c56739722a55b15215b9bd6743d9304361
smile995 <ah.smile995@gmail.com>
2025-01-21 21:47:07 +0600

    Leanring about number in python

---
+++ number.py
@@ -0,0 +1,42 @@
+import random
+# int types of number
+positiveNumber = 35656222554887711
+negativeNumber = -3255522
+# print(type(positiveNumber))
+# print(type(negativeNumber))
+
+# float types of number
+positiveFloat = 1.10
+negetiveFloat = -35.59
+# print(type(positiveFloat))
+# print(type(negetiveFloat))
+
+# complex types of number
+positiveComplex = 3+5j
+negetivecoplex = -5j
+
+# print(type(positiveComplex))
+# print(type(negetivecoplex))
+
+# Type Conversion Exampel
+
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+# print(x,y,z)
+
+#convert from int to float:
+a = float(x)
+
+#convert from float to int:
+b = int(y) 
+
+#convert from int to complex:
+c = complex(x)
+
+# print(a)
+# print(b)
+# print(c)
+
+randomValue= random.randrange(1,100) 
+print(randomValue) #every time print different number
