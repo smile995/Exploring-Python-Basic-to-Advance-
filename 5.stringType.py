1b7f4854625716e47902dd45e550ec599064f0bf
smile995 <ah.smile995@gmail.com>
2025-01-21 22:24:17 +0600

    Exploring about string and that's methods

---
+++ 5.stringType.py
@@ -0,0 +1,14 @@
+# single line string
+name= "Amir Hamza Ismail" #using double quatation
+movie='The Gangstar,The Police' #using single quatation
+# Multi line string
+about= """Myself Amir Hamza Ismail,
+I'm student of CCNUST,
+Department of CSE,
+Loved to explore new technology""" #Triple double quates
+# print(about)
+
+hobby='''Loved to watch islamic revulation seris or something,
+In leisure, like to train my boddy to make fit,
+Playing cricket and football ''' #Triple single quates
+# print(hobby)
