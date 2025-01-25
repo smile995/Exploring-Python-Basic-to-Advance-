8b2fda4cbb3cd0cbe45a7cf2fb85a6f4f8ac1340
smile995 <ah.smile995@gmail.com>
2025-01-25 23:06:25 +0600

    Explore python Arithmetic Operators

---
+++ 6.operator.py
@@ -0,0 +1,11 @@
+# Python Arithmetic Operators
+x=23
+y=5
+sum= x+y #Addition ==> 28
+sub= x-y #Subtraction ==>18
+mul=x*y  #Multiplication ==>115
+div=x/y  #Division ==>4.6
+mod= x%y  #Modulus ==>28
+expo= x**y #Exponentiation ==>6436343
+Fdiv= x//y #Floor division ==>4
+
