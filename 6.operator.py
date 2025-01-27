9bed37eb96fc5a129561ed898ff98f7a0daa5194
smile995 <ah.smile995@gmail.com>
2025-01-27 22:09:36 +0600

    Take a challenge to explore basic python in a day

--- 6.operator.py
+++ 6.operator.py
@@ -9,3 +9,4 @@ mod= x%y  #Modulus ==>28
 expo= x**y #Exponentiation ==>6436343
 Fdiv= x//y #Floor division ==>4
 
+# To do=> somtihing will be added
---
+++ mission.py
@@ -0,0 +1,5 @@
+# Mission is complete basic python in a day
+name= input("Enter your name here: ")
+age= input("Enter your age: ")
+message= f"Mr.{name} you are {age} year old now"
+print(message)
