4a27f6388608eb83bfbfad482eeee9bc768e0d91
smile995 <ah.smile995@gmail.com>
2025-02-01 23:02:16 +0600

    Practice listing loop

--- 7.list.py
+++ 7.list.py
@@ -5,4 +5,25 @@ banana=fruits[1]
 grape= fruits[-1] # Grapes ===> negetive value means start from last
 mango_to_orange= fruits[:5]
 apple_to_last= fruits[2:]
-print(apple_to_last)
+# print(apple_to_last)
+# Changing list property
+fruits[0]="tomato" #mango change tomato
+# Append as child (push)
+fruits.append("Black-berry") # append method add a item in an list at last index
+fruits.insert(3, "Litchi") # insert method add a item in selected index
+
+
+# Removing property from list
+fruits.remove("Kiwi") #Kiwi is removed from list
+fruits.pop(2) #remove by index. apple will be remove
+
+# deleting property completely from list
+thislist = ["apple", "banana", "cherry"]
+del thislist[1]
+print(thislist.clear()) #clear() make empty a list 
+
+for fruit in fruits:
+    print(fruit)
+
+for fruit in range(len(fruits)):
+    print(fruit)
