ae63277f7a741b01d200ea594b37b19ce9fa79b6
smile995 <ah.smile995@gmail.com>
2025-02-01 22:38:34 +0600

    Accesing list items

--- 7.list.py
+++ 7.list.py
@@ -1,2 +1,8 @@
-fruits=["Mango","Banana", "Apple", "Pine-apple"]
-print(fruits)
+fruits=["Mango","Banana", "Apple", "Pine-apple","Orange","Kiwi","Grapes"]
+# list property access by index
+mango=fruits[0]
+banana=fruits[1]
+grape= fruits[-1] # Grapes ===> negetive value means start from last
+mango_to_orange= fruits[:5]
+apple_to_last= fruits[2:]
+print(apple_to_last)
