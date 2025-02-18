83212710dc1aa0bacef2a6b8c1516eb7946a251d
smile995 <ah.smile995@gmail.com>
2025-02-18 23:41:58 +0600

    Exploring python for loop

---
+++ 13.forLoop.py
@@ -0,0 +1,16 @@
+# how initialized the for loop in an array
+fruits=["mango","banana","litchi","apple","orange"]
+for fruit in fruits:
+    if fruit=="banana":
+        print(fruit)
+        if "a" in fruit:
+            print("vowel")
+        continue
+    
+# how to loop in a string
+name="Amir Hamza Ismail"
+for letter in name:
+    if letter=="i":
+        print("break")
+        break
+        
