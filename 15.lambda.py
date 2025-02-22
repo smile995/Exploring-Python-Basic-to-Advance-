42acc5f37905dd88dec105a0a40df597e0aad328
smile995 <ah.smile995@gmail.com>
2025-02-22 22:56:47 +0600

    Explore and practice python lambda function

---
+++ 15.lambda.py
@@ -0,0 +1,11 @@
+# lambda arguments : expression
+x= lambda number: number+10
+value_x=x(15)
+print(value_x)
+
+# ______________________________
+# make a lambda function to add five digits
+
+sum_calc= lambda a,b,c,d,e: a+b+c+d+e
+result= sum_calc(2,343,4,43,545)
+print(result)
